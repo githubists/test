df1b239b461e44057d68dd1546d2cb32fdf98733
veveve bobobo <thoftbank@gmail.com>
2018-12-02 12:50:38 +0900

    py added

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from argparse import ArgumentParser
+
+
+from io import BytesIO
+
+from flask import Flask, request, abort, render_template, Response, redirect, url_for
+
+import urllib
+
+
+
+
+app = Flask(__name__, template_folder = '01_HTML')
+
+
+
+
+#スリープ回避
+@app.route("/wakeup", methods=['GET'])
+def wakeup():
+    return "woke up!!!!"
+
+
+#アップロードサイトのURL
+@app.route('/', methods=['GET'])
+def get():
+    title = "TITLE"
+    return render_template("about_changed.html")
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
+
