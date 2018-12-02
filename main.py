8b038110cda18870d707c132ce229e5b509af0f4
veveve bobobo <thoftbank@gmail.com>
2018-12-02 14:26:39 +0900

    procfile initialized

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ from io import BytesIO
 
 from flask import Flask, request, abort, render_template, Response, redirect, url_for
 
+
 import urllib
 
 
 
 
-app = Flask(__name__, template_folder = 'html')
+"""app = Flask(__name__, template_folder = 'html')"""
+app = Flask(__name__)
 
 
-
-#スリープ回避
-@app.route("/wakeup", methods=['GET'])
+"""
+@app.route("/", methods=['GET'])
 def wakeup():
     return "woke up!!!!"
 
 
+
+
+"""
 #アップロードサイトのURL
 @app.route('/', methods=['GET'])
 def get():
@@ -30,9 +34,11 @@ def get():
 
 
 
-
+"""
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
 
