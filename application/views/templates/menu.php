6432035a875447d51e85961794cae92afea1f529
veveve bobobo <thoftbank@gmail.com>
2019-05-11 22:24:37 +0900

    リンクを貼るなど

--- application/views/templates/menu.php
+++ application/views/templates/menu.php
@@ -32,6 +32,7 @@
             </li>
             <!-- End Dropdown-inline -->
             <li class="ampstart-nav-item relative "><a href="/">トップ</a></li>
+            <li class="ampstart-nav-item realtive"><a href="/about">代表挨拶</a></li>
             <li class="ampstart-nav-item relative "><a href="/map">館内案内図</a></li>
             <li class="ampstart-nav-item relative "><a href="/access">アクセス</a></li>
             <li class="ampstart-nav-item relative "><a href="/timetable">タイムテーブル</a></li>
@@ -46,10 +47,5 @@
           <a href="https://www.instagram.com/utmed_mayfes/" target="_blank" rel="noopener" class="inline-block p1" aria-label="東大医学部五月祭企画公式Instagramアカウントへのリンク"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 54 54"><title>instagram</title><path d="M27.2 6.1c-5.1 0-5.8 0-7.8.1s-3.4.4-4.6.9c-1.2.5-2.3 1.1-3.3 2.2-1.1 1-1.7 2.1-2.2 3.3-.5 1.2-.8 2.6-.9 4.6-.1 2-.1 2.7-.1 7.8s0 5.8.1 7.8.4 3.4.9 4.6c.5 1.2 1.1 2.3 2.2 3.3 1 1.1 2.1 1.7 3.3 2.2 1.2.5 2.6.8 4.6.9 2 .1 2.7.1 7.8.1s5.8 0 7.8-.1 3.4-.4 4.6-.9c1.2-.5 2.3-1.1 3.3-2.2 1.1-1 1.7-2.1 2.2-3.3.5-1.2.8-2.6.9-4.6.1-2 .1-2.7.1-7.8s0-5.8-.1-7.8-.4-3.4-.9-4.6c-.5-1.2-1.1-2.3-2.2-3.3-1-1.1-2.1-1.7-3.3-2.2-1.2-.5-2.6-.8-4.6-.9-2-.1-2.7-.1-7.8-.1zm0 3.4c5 0 5.6 0 7.6.1 1.9.1 2.9.4 3.5.7.9.3 1.6.7 2.2 1.4.7.6 1.1 1.3 1.4 2.2.3.6.6 1.6.7 3.5.1 2 .1 2.6.1 7.6s0 5.6-.1 7.6c-.1 1.9-.4 2.9-.7 3.5-.3.9-.7 1.6-1.4 2.2-.7.7-1.3 1.1-2.2 1.4-.6.3-1.7.6-3.5.7-2 .1-2.6.1-7.6.1-5.1 0-5.7 0-7.7-.1-1.8-.1-2.9-.4-3.5-.7-.9-.3-1.5-.7-2.2-1.4-.7-.7-1.1-1.3-1.4-2.2-.3-.6-.6-1.7-.7-3.5 0-2-.1-2.6-.1-7.6 0-5.1.1-5.7.1-7.7.1-1.8.4-2.8.7-3.5.3-.9.7-1.5 1.4-2.2.7-.6 1.3-1.1 2.2-1.4.6-.3 1.6-.6 3.5-.7h7.7zm0 5.8c-5.4 0-9.7 4.3-9.7 9.7 0 5.4 4.3 9.7 9.7 9.7 5.4 0 9.7-4.3 9.7-9.7 0-5.4-4.3-9.7-9.7-9.7zm0 16c-3.5 0-6.3-2.8-6.3-6.3s2.8-6.3 6.3-6.3 6.3 2.8 6.3 6.3-2.8 6.3-6.3 6.3zm12.4-16.4c0 1.3-1.1 2.3-2.3 2.3-1.3 0-2.3-1-2.3-2.3 0-1.2 1-2.3 2.3-2.3 1.2 0 2.3 1.1 2.3 2.3z" class="ampstart-icon ampstart-icon-instagram"></path></svg></a>
           <a href="mailto:mayfes2019.utmed@gmail.com" target="_blank" rel="noopener" class="inline-block p1" aria-label="東大医学部五月祭企画公式e-mailアドレスへのリンク"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="18.4" viewBox="0 0 56 43"><title>email</title><path d="M10.5 6.4C9.1 6.4 8 7.5 8 8.9v21.3c0 1.3 1.1 2.5 2.5 2.5h34.9c1.4 0 2.5-1.2 2.5-2.5V8.9c0-1.4-1.1-2.5-2.5-2.5H10.5zm2.1 2.5h30.7L27.9 22.3 12.6 8.9zm-2.1 1.4l16.6 14.6c.5.4 1.2.4 1.7 0l16.6-14.6v19.9H10.5V10.3z" class="ampstart-icon ampstart-icon-email"></path></svg></a>
   </b>
-
-      <ul class="ampstart-sidebar-faq m0 px3">
-        <li class="ampstart-faq-item"><a href="/about" class="text-decoration-none">About</a></li>
-        <li class="ampstart-faq-item"><a href="mailto:mayfes2019.utmed@gmail.com" class="text-decoration-none">Contact</a></li>
-      </ul>
   </amp-sidebar>
   <!-- End Sidebar -->
