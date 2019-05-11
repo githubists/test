73a08bc4c1ff96992d3be8e34f101a6f584d2473
Henri Weyl <29483898+henry-weyl@users.noreply.github.com>
2019-05-11 11:09:56 +0900

    Update timetable.php

--- application/views/pages/timetable.php
+++ application/views/pages/timetable.php
@@ -8,88 +8,80 @@
   <meta property="og:image" content="https://utmed-mayfes.net/img/header/header-og-1x-50-withtext.jpg" />
   <meta property="og:description" content="東京大学 第92回五月祭 医学部企画『医へのいざない』の公式ウェブサイトです。" />
 <?=$header?>
+img{
+  height:auto;
+}
+.timetable-img button{
+  position:absolute
+}
+.timetable-img>nav{
+  width:auto;
+  height:auto
+}
 <?=$menu?>
 <?=$navbar?>
   <main id="content" role="main" class="">
     <article class="px2">
       <section id="timetable" class="relative">
         <h2 class="relative header-about">タイムテーブル</h2>
-        <div style="text-align:center;">
-        <img src="/img/timetable/timetable_2.jpg" usemap="#Map" width=auto height="600" layout="fixed_height" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups">
-        </div>
-          <map name="Map">
-          <nav class="dock flex flex-wrap items-start justify-around absolute">
-            <button on="tap:lecture">
-              <area shape="rect" coords="132.4, 81.2, 208.4, 216.2" alt="講演会">
-              <area shape="rect" coords="132.4, 458.3, 208.4, 566.4" alt="講演会">
-              <area shape="rect" coords="353.8, 83.8, 434, 184.1" alt="講演会">
-              <area shape="rect" coords="353.8, 506.4, 434, 569" alt="講演会">
-            </button>
-            <button on="tap:chamber">
-              <area shape="rect" coords="132.4, 223, 208.4, 453.6" alt="室内楽">
-            </button>
-            <button on="tap:piano">
-              <area shape="rect" coords="353.8, 196, 434, 499.7" alt="ピアノ">
-            <button on="tap:muscle">
-              <area shape="rect" coords="25.8, 154.7, 106, 183.6" alt="筋肉">
-              <area shape="rect" coords="25.8, 287.1, 106, 316" alt="筋肉">
-              <area shape="rect" coords="25.8, 409.7, 106, 438.6" alt="筋肉">
-              <area shape="rect" coords="25.8, 505.3, 106, 534.3" alt="筋肉">
-              <area shape="rect" coords="248.3, 154.7, 328.4, 183.6" alt="筋肉">
-              <area shape="rect" coords="248.3, 287.1, 328.4, 316" alt="筋肉">
-              <area shape="rect" coords="248.3, 409.7, 328.4, 438.6" alt="筋肉">
-              <area shape="rect" coords="248.3, 505.3, 328.4, 534.3" alt="筋肉">
-            </button>
-            <button on="tap:research">
-              <area shape="rect" coords="25.8, 220.9, 106, 249.8" alt="最先端">
-              <area shape="rect" coords="25.8, 352.8, 106, 381.7" alt="最先端">
-              <area shape="rect" coords="25.8, 457.2, 106, 486.2" alt="最先端">
-              <area shape="rect" coords="248.3, 220.9, 328.4, 249.8" alt="最先端">
-              <area shape="rect" coords="248.3, 352.8, 328.4, 391.55" alt="最先端">
-              <area shape="rect" coords="248.3, 457.2, 328.4, 486.2" alt="最先端">
-            </button>
-            <button on="tap:narrative">
-              <area shape="rect" coords="25.8, 381.2, 106, 410.2" alt="公衆衛生">
-              <area shape="rect" coords="248.3, 381.2, 328.4, 410.2" alt="公衆衛生">
-            <!--<button on="tap:seiriken.scrollTo(duration=200)">
-              <area shape="rect" coords="50.625,90.75,105.75,156" alt=整理券>
-            </button>-->
+        <div class="col-12 sm-col-8 md-col-7 center mx-auto relative timetable-img">
+          <amp-img src="/img/timetable/timetable_2.jpg" width="455" height="600" layout="responsive" class=""></amp-img>
+          <nav class="absolute top-0 right-0 bottom-0 left-0">
+            <button on="tap:lecture" style="left:29.10%;top:13.53%;width:16.70%;height:22.50%"></button>
+            <button on="tap:lecture" style="left:29.10%;top:76.38%;width:16.70%;height:18.02%"></button>
+            <button on="tap:lecture" style="left:77.76%;top:13.97%;width:17.63%;height:16.72%"></button>
+            <button on="tap:lecture" style="left:77.76%;top:84.40%;width:17.63%;height:10.43%"></button>
+            <button on="tap:chamber" style="left:29.10%;top:37.17%;width:16.70%;height:38.43%"></button>
+            <button on="tap:piano" style="left:77.76%;top:32.67%;width:17.63%;height:50.62%"></button>
+            <button on="tap:muscle" style="left:5.67%;top:25.78%;width:17.63%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:5.67%;top:47.85%;width:17.63%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:5.67%;top:68.28%;width:17.63%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:5.67%;top:84.22%;width:17.63%;height:4.83%"></button>
+            <button on="tap:muscle" style="left:54.57%;top:25.78%;width:17.60%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:54.57%;top:47.85%;width:17.60%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:54.57%;top:68.28%;width:17.60%;height:4.82%"></button>
+            <button on="tap:muscle" style="left:54.57%;top:84.22%;width:17.60%;height:4.83%"></button>
+            <button on="tap:research" style="left:5.67%;top:36.82%;width:17.63%;height:4.82%"></button>
+            <button on="tap:research" style="left:5.67%;top:58.80%;width:17.63%;height:4.82%"></button>
+            <button on="tap:research" style="left:5.67%;top:76.20%;width:17.63%;height:4.83%"></button>
+            <button on="tap:research" style="left:54.57%;top:36.82%;width:17.60%;height:4.82%"></button>
+            <button on="tap:research" style="left:54.57%;top:58.80%;width:17.60%;height:6.46%"></button>
+            <button on="tap:research" style="left:54.57%;top:76.20%;width:17.60%;height:4.83%"></button>
+            <button on="tap:narrative" style="left:5.67%;top:63.53%;width:17.63%;height:4.83%"></button>
+            <button on="tap:narrative" style="left:54.57%;top:63.53%;width:17.60%;height:4.83%"></button>
           </nav>
-          </map>
+        </div>
       </section>
 
       <section id="seiriken" class="relative">
         <h2 class="relative header-about">整理券について</h2>
         <p class="m2 relative p2">
-          今年度、五月祭医学部企画では混雑緩和と、より一層ご満足いただける体験のため、一部企画で整理券を配布いたします。<br>
-          一部企画にご参加いただく際には、当日配布されます整理券が必須となりますのでご了承ください。<br>
+          今年度、五月祭医学部企画では混雑緩和と、より一層ご満足いただける体験のため、一部企画で整理券を配布いたします。<br />
+          一部企画にご参加いただく際には、当日配布されます整理券が必須となりますのでご了承ください。<br />
         </p>
-        <u><h3 class="relative py2" id="taisyou">整理券配布対象企画</h3></u>
-        <div class="pl2">
-          ・手術体験（開腹手術・腹腔鏡手術）<br>
-          ・血圧測定体験<br>
-          ・動脈観察<br>
-          ・VR医学体験<br>
-          ・救急救命体験<br>
-        <br>
+        <h3 class="relative py2 underline" id="taisyou">整理券配布対象企画</h3>
+        <ul class="list-disc">
+          <li>・手術体験（開腹手術・腹腔鏡手術）</li>
+          <li>・血圧測定体験</li>
+          <li>・動脈観察</li>
+          <li>・VR医学体験</li>
+          <li>・
+            救急救命体験</li>
+        </ul>
         企画ごとに集合場所が異なりますのでご注意ください。
-        </div>
-        <u><h3 class="relative py2">整理券の配布場所と入場までの流れについて</h3></u>
+        <h3 class="relative py2 underline">整理券の配布場所と入場までの流れについて</h3>
         <div class="pl2">
-          18日、19日の両日で、その日ご入場いただける整理券配布対象企画の整理券を、企画別の配布列を作って配布いたします。<br>
-          先着順で、集合時刻の書かれた整理券を配布いたします。<br>
-          整理券をお持ちのお客様は、集合時刻に所定の場所にお越しください。その後、スタッフの案内に従ってご体験いただきます。<br>
-          <img src=""><br>
-          なお、整理券の枚数につきましては、以下の通りです。<br>
-          <div style="text-align:center;">
-          <img src="/img/timetable/maisuu.jpg" width=auto height="175" layout="fixed_height"><br>
-          </div>
+          18日、19日の両日で、その日ご入場いただける整理券配布対象企画の整理券を、企画別の配布列を作って配布いたします。<br />
+          先着順で、集合時刻の書かれた整理券を配布いたします。<br />
+          整理券をお持ちのお客様は、集合時刻に所定の場所にお越しください。その後、スタッフの案内に従ってご体験いただきます。<br />
+          なお、整理券の枚数につきましては、以下の通りです。<br />
+          <amp-img src="/img/timetable/maisuu.jpg" width="327" height="103" layout="responsive"></amp-img><br />
           <div class="pt2">①手術体験、血圧測定、動脈観察、VR医学体験</div>
           <div class="pl2">
-            1枚の整理券につき体験者1名（手術体験・VR医学体験は小学生以上）＋同伴者2名（ただし、未就学児を同伴者に含む場合は3名）までご入場いただけます。<br>
-            例1：体験者1名と同伴の大人2名<br>
-            例2：体験者1名と同伴の大人1名と同伴の未就学児2名<br>
-            <br>
+            1枚の整理券につき体験者1名（手術体験・VR医学体験は小学生以上）＋同伴者2名（ただし、未就学児を同伴者に含む場合は3名）までご入場いただけます。<br />
+            例1：体験者1名と同伴の大人2名<br />
+            例2：体験者1名と同伴の大人1名と同伴の未就学児2名<br />
+            <br />
             2名以上での体験をご希望の場合、整理券を体験者の数だけお取りください。
           </div>
           <div class="pt2">②救命救急体験</div>
@@ -97,41 +89,37 @@
             中学生以上の体験者1名につき1枚の配布となります。小学生以下の方は難易度の関係からご体験いただけません。また、付き添い、見学のためであっても整理券をお持ちでない方の入場はできませんのであらかじめご了承ください。
           </div>
         </div>
-        <u><h3 class="relative py2">配布時刻</h3></u>
+        <h3 class="relative py2 underline">配布時刻</h3>
         <div class="pl2">
-          両日9:00, 12:00, 15:00の3回配布いたします。<br>
-          なお、予定枚数に達し次第配布を終了させていただきます。<br>
-          12:00の回、15:00の回の配布列形成はそれぞれ11:30, 14:30からとなります。<br>
-          それより前の列形成は禁止させていただいております。ご了承ください。<br>
-          <br>
+          両日9:00, 12:00, 15:00の3回配布いたします。<br />
+          なお、予定枚数に達し次第配布を終了させていただきます。<br />
+          12:00の回、15:00の回の配布列形成はそれぞれ11:30, 14:30からとなります。<br />
+          それより前の列形成は禁止させていただいております。ご了承ください。<br />
+          <br />
           なお、医学部本館は両日朝9:00より会場予定です。開場後、スタッフの支持に従って配布列への整列にご協力ください。
         </div>
-        <u><h3 class="relative py2">企画への参加について</h3></u>
+        <h3 class="relative py2 undeline">企画への参加について</h3>
         <div class="">①手術体験、血圧測定、動脈観察、VR医学体験</div>
         <div class="pl2">整理券に表示されている時刻になりましたら階段を上がって左手側の2F総合実習室改札口へお越しいただき、お持ちの整理券をスタッフにお見せください。</div>
         <div class="pt2">②救命救急体験</div>
         <div class="pl2">整理券の時刻までに、3Fにへの階段を上がって左側にあります救命救急体験までお越しください。</div>
-        <div style="text-align:center;">
-        <img src="/img/timetable/ticket_map.jpg" width=auto height="600" layout="fixed_height"><br>
-        </div>
-        <u><h3 class="relative py2">注意事項</h3></u>
-        <div class="pl2">
-          <ul style="list-style:disc;">
-            <li>・各企画の配布数が上限に達し次第、配布を終了させていただきます。列にお並びいただいても整理券を発行できない場合がございます。</li>
-            <li>・整理券に記載された時刻を過ぎた場合にはご入場いただけない場合がございます。</li>
-            <li>・入場順は整理券の配布順ではございません。</li>
-            <li>・スタッフの指示に従った整列・移動にご協力をお願いいたします。</li>
-            <li>・列にお並びの際は、広がったり順路から外れたりせず、他のお客様のご迷惑とならないようご配慮お願いいたします。</li>
-            <li>・いかなる場合でも配布後の整理券の交換・再発行はできません。</li>
-            <li>・お客様同士のトラブルなどに関しては、一切の責任を負いかねます。</li>
-            <li>・代表者のみが並んでいただいても構いません。</li>
-            <li>・整理券の譲渡、交換、転売、コピー、偽造行為は固くお断りします。</li>
-            <li>・館内は通路が狭くなっておりますので、走ったり、押し合ったりすることなく譲り合ってお進みください。</li>
-            <li>・危険や混乱が予想される場合、整理券の配布を中断、お断りする場合もございます。</li>
-            <li>・天災、交通機関の不全、設備の不具合などのやむをえない事情で企画を中止、もしくは企画内容を変更する場合がございます。これらの場合でも、補償に応じることはできません。</li>
-            <li>・以上の内容は変更になる可能性がございます。</li>
-          </ul>
-        </div>
+        <amp-img src="/img/timetable/ticket_map.jpg" width="554" height="600" layout="responsive" class="mx-auto"></amp-img><br />
+        <h3 class="relative py2 underline">注意事項</h3>
+        <ul class="pl2">
+          <li>・各企画の配布数が上限に達し次第、配布を終了させていただきます。列にお並びいただいても整理券を発行できない場合がございます。</li>
+          <li>・整理券に記載された時刻を過ぎた場合にはご入場いただけない場合がございます。</li>
+          <li>・入場順は整理券の配布順ではございません。</li>
+          <li>・スタッフの指示に従った整列・移動にご協力をお願いいたします。</li>
+          <li>・列にお並びの際は、広がったり順路から外れたりせず、他のお客様のご迷惑とならないようご配慮お願いいたします。</li>
+          <li>・いかなる場合でも配布後の整理券の交換・再発行はできません。</li>
+          <li>・お客様同士のトラブルなどに関しては、一切の責任を負いかねます。</li>
+          <li>・代表者のみが並んでいただいても構いません。</li>
+          <li>・整理券の譲渡、交換、転売、コピー、偽造行為は固くお断りします。</li>
+          <li>・館内は通路が狭くなっておりますので、走ったり、押し合ったりすることなく譲り合ってお進みください。</li>
+          <li>・危険や混乱が予想される場合、整理券の配布を中断、お断りする場合もございます。</li>
+          <li>・天災、交通機関の不全、設備の不具合などのやむをえない事情で企画を中止、もしくは企画内容を変更する場合がございます。これらの場合でも、補償に応じることはできません。</li>
+          <li>・以上の内容は変更になる可能性がございます。</li>
+        </ul>
       </section>
     </article>
   </main>
