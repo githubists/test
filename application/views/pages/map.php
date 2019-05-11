5dbf3dc5ed9ffec047c3bfe69b8196c28049807f
Henri Weyl <29483898+henry-weyl@users.noreply.github.com>
2019-05-11 16:09:12 +0900

    Update map.php

--- application/views/pages/map.php
+++ application/views/pages/map.php
@@ -4,7 +4,7 @@
   <meta name="description" content="東京大学 第92回五月祭 医学部企画『医へのいざない』の公式ウェブサイトです。2019年は第71回目で、今年もクイズ・ゲーム・学術展示等面白い企画を沢山ご用意しております。皆様のご来場をお待ちしております。"/>
   <meta property="og:title" content="館内案内 | 東大医学部 五月祭企画 医へのいざない" />
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://utmed-mayfes.net/timetable" />
+  <meta property="og:url" content="https://utmed-mayfes.net/map" />
   <meta property="og:image" content="https://utmed-mayfes.net/img/header/header-og-1x-50-withtext.jpg" />
   <meta property="og:description" content="東京大学 第92回五月祭 医学部企画『医へのいざない』の館内案内図です。" />
 <?=$header?>
