cd39d08747a7216610f589c6c7dddbc7c816a45e
veveve bobobo <thoftbank@gmail.com>
2019-05-16 19:32:55 +0900

    mayfesawards投票ページに内容追加

--- application/views/pages/mayfesawards.php
+++ application/views/pages/mayfesawards.php
@@ -32,7 +32,7 @@
     line-height: 2rem;
     margin-left: 1%;
     margin-right: 1%;
-    box-shadow: 4px 4px 8px silver;
+    box-shadow: 5px 5px 10px silver;
     background: white;
 }
 button.vote-link{
@@ -40,9 +40,12 @@ button.vote-link{
   width:auto;
   text-decoration:underline;
 }
-button.vote-link:hover{
+button.vote-link:hover, a:hover, a:active{
   transition: all 1s;
   color: #ffaa22;
+}
+.red{
+  color:red
 }
 #project-id{
   font-size:2rem;
@@ -51,6 +54,35 @@ button.vote-link:hover{
 #mobile, #office{
   padding-top:60px;
   margin-top:-60px;
+}
+.vote{
+  /*border-top:solid 4px grey;*/
+}
+.heading-vote {
+  position: relative;
+  padding-left: 25px;
+}
+.heading-vote:before {
+  position: absolute;
+  content: '';
+  bottom: -3px;
+  left: 0;
+  width: 0;
+  height: 0;
+  border: none;
+  border-left: solid 15px transparent;
+  border-bottom: solid 15px rgb(119, 195, 223);
+}
+.heading-vote:after {
+  position: absolute;
+  content: '';
+  bottom: -3px;
+  left: 10px;
+  width: 100%;
+  border-bottom: solid 3px rgb(119, 195, 223);
+}
+section>p{
+  box-shadow:none;
 }
 
 <?=$menu?>
@@ -64,21 +96,35 @@ button.vote-link:hover{
         <button class="vote-link"on="tap:mobile.scrollTo(duration=200)">＜スマホで投票＞</button>
         あるいは
         <button class="vote-link"on="tap:office.scrollTo(duration=200)">＜案内所にて投票＞</button>
-        の手順に従い、当企画への投票を行っていただければ幸いです。ご協力よろしくお願いいたします。投票期間は
-        <br>・18日(土) 10:00 ~ 18:00
-        <br>・19日(日) 10:00 ~ 15:45
+        の手順に従い、当企画<strong>（企画ID: 565）</strong>への投票を行っていただければ幸いです。ご協力よろしくお願いいたします。投票期間は
+        <strong>
+          <br>・18日(土) 10:00 ~ 18:00
+          <br>・19日(日) 10:00 ~ <span class="red">15:45</span>
+        </strong>
         <br>です。1日1回、3企画までご投票いただけます。案内所は大変混雑することが予想されます。スマホ投票でしたらスムーズに投票することができます。
-        <br>
-        <br>
-        <span id="project-id"><small>企画ID: </small><strong>565</strong></span>
+        
       </p>
     </section>
-    <section id="mobile">
-      <h3>スマホで投票</h3>
-    </section>
-    <section id="office">
-      <h3>案内所にて投票</h3>
-    </section>
+    <div id="mobile">
+      <section class="mt2 p2 box1 vote">
+        <h3 class="heading-vote">スマホで投票</h3>
+        <p class="mt1">
+        投票用 LINE@ アカウントをご登録いただくと、案内所に行かずにその場で投票できます。<a class="inline-block"href="https://line.me/R/ti/p/nT2Bp1ufat">＜こちらのページ＞</a>を読み込み、LINEからご投票ください。
+        <br>
+        <br>
+        <span id="project-id"><small>企画ID: </small><strong>565</strong></span></p>
+      </section>
+    </div>
+    <div id="office" class="mb2">
+      <section class="mt2 p2 box1 vote">
+        <h3 class="heading-vote">案内所にて投票</h3>
+        <p class="mt1">
+          正門案内所・赤門案内所・農学部案内所のいずれかにお越しいただくことで投票できます。投票の特典として福引きにご参加いただけます。
+          <br>
+          <br>
+          <span id="project-id"><small>企画ID: </small><strong>565</strong></span></p>
+      </section>
+    </div>
   </article>
 </main>
 <?=$footer?>
